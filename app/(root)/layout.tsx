cf3466c6e991d01eed7515e6e2f9e8fa5024d4e9
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-03-18 22:30:15 +0200

    layout sidebar & mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from "react";
+import { Sidebar } from "@/components/shared/sidebar";
+import { MobileNav } from "@/components/shared/mobile-nav";
 
 interface Props {
   children: ReactNode;
@@ -7,6 +9,8 @@ interface Props {
 export default function Layout({ children }: Props) {
   return (
     <main className="root">
+      <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
